f76b6ff1fc5f408483d3b8407a10390c34abd7be
Chan Ryu <upriser@gmail.com>
2020-12-12 14:17:04 +1100

    Make Firebase demo list sortable (package:provider added) (#4)
    Co-authored-by: Chan Ryu <cryu@snapchat.com>

--- lib/sandbox/firebase/firebase_sandbox.dart
+++ lib/sandbox/firebase/firebase_sandbox.dart
@@ -1,27 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FirebaseSandbox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Firebase Sandbox'),
+    return ChangeNotifierProvider<_StateChangeNotifier>(
+      create: (_) => _StateChangeNotifier(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Firebase Sandbox'),
+          actions: [
+            _SortButton(),
+          ],
+        ),
+        body: ProgrammingLanguageList(),
       ),
-      body: ProgrammingLanguageList(orderBy: 'year'),
     );
   }
 }
 
-class ProgrammingLanguage {
+class _SortButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PopupMenuButton<String>(
+      icon: Icon(Icons.sort_rounded),
+      onSelected: (String orderBy) {
+        final state = context.read<_StateChangeNotifier>();
+        state.orderBy = orderBy;
+      },
+      itemBuilder: (BuildContext context) {
+        final state = context.read<_StateChangeNotifier>();
+        final List<String> choices = ['name', 'year'];
+        return <PopupMenuItem<String>>[
+          for (final choice in choices)
+            PopupMenuItem<String>(
+              value: choice,
+              child: Row(
+                children: [
+                  Opacity(
+                    opacity: state.orderBy == choice ? 1 : 0,
+                    child: Icon(
+                      Icons.check_rounded,
+                      color: Colors.black87,
+                    ),
+                  ),
+                  SizedBox(width: 8),
+                  Text(
+                    choice[0].toUpperCase() + choice.substring(1), // Captialize
+                    style: TextStyle(
+                        fontWeight: state.orderBy == choice
+                            ? FontWeight.bold
+                            : FontWeight.normal),
+                  ),
+                ],
+              ),
+            ),
+        ];
+      },
+    );
+  }
+}
+
+class _StateChangeNotifier extends ChangeNotifier {
+  String _orderBy = 'year';
+
+  String get orderBy => _orderBy;
+  set orderBy(String orderBy) {
+    _orderBy = orderBy;
+    notifyListeners();
+  }
+}
+
+class ProgrammingLanguageModel {
   final String name;
   final String designers;
   final int year;
   final String logoURL;
 
-  ProgrammingLanguage({this.name, this.designers, this.year, this.logoURL});
+  ProgrammingLanguageModel(
+      {this.name, this.designers, this.year, this.logoURL});
 
-  factory ProgrammingLanguage.fromDocument(DocumentSnapshot snapshot) {
+  factory ProgrammingLanguageModel.fromDocument(DocumentSnapshot snapshot) {
     String name;
     String designers;
     int year;
@@ -35,7 +95,7 @@ class ProgrammingLanguage {
     } on StateError catch (e) {
       print(e);
     }
-    return ProgrammingLanguage(
+    return ProgrammingLanguageModel(
       name: name,
       designers: designers,
       year: year,
@@ -45,19 +105,13 @@ class ProgrammingLanguage {
 }
 
 class ProgrammingLanguageList extends StatelessWidget {
-  final String orderBy;
-
-  ProgrammingLanguageList({Key key, this.orderBy}) : super(key: key);
-
-  static const kListItemTitleTextStyle =
-      TextStyle(fontSize: 17, fontWeight: FontWeight.bold);
+  ProgrammingLanguageList({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final orderBy = context.watch<_StateChangeNotifier>();
     final collectionRef = FirebaseFirestore.instance.collection('languages');
-    final snapshotStream = orderBy == null
-        ? collectionRef.snapshots()
-        : collectionRef.orderBy(orderBy).snapshots();
+    final snapshotStream = collectionRef.orderBy(orderBy.orderBy).snapshots();
 
     return StreamBuilder(
       stream: snapshotStream,
@@ -74,16 +128,33 @@ class ProgrammingLanguageList extends StatelessWidget {
         return ListView.separated(
           itemCount: docs.length,
           itemBuilder: (context, index) {
-            final lang = ProgrammingLanguage.fromDocument(docs[index]);
-            return ListTile(
-              leading: Image.network(lang.logoURL, width: 48, height: 48),
-              title: Text(lang.name, style: kListItemTitleTextStyle),
-              subtitle: Text('Since ${lang.year} - ${lang.designers}'),
-            );
+            final model = ProgrammingLanguageModel.fromDocument(docs[index]);
+            return ProgrammingLanguageListTile(model: model);
           },
           separatorBuilder: (context, index) => Divider(indent: 16),
         );
       },
+    );
+  }
+}
+
+class ProgrammingLanguageListTile extends StatelessWidget {
+  const ProgrammingLanguageListTile({
+    Key key,
+    @required this.model,
+  }) : super(key: key);
+
+  static const kListItemTitleTextStyle =
+      TextStyle(fontSize: 17, fontWeight: FontWeight.bold);
+
+  final ProgrammingLanguageModel model;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Image.network(model.logoURL, width: 48, height: 48),
+      title: Text(model.name, style: kListItemTitleTextStyle),
+      subtitle: Text('Since ${model.year} - ${model.designers}'),
     );
   }
 }
